2453bd01aa0c874c6ea1babba74f38816951547d
Adam Moyle <adam@happy.co>
2020-08-03 15:10:21 +0930

    Change `value` variable to be `storedValue`

--- ios/Classes/AtomicInteger.swift
+++ ios/Classes/AtomicInteger.swift
@@ -9,28 +9,28 @@ import Foundation
 
 public final class AtomicInteger {
     private let lock = DispatchSemaphore(value: 1)
-    private var localValue: Int
+    private var storedValue: Int
 
     public init(value initialValue: Int = 0) {
-        localValue = initialValue
+        storedValue = initialValue
     }
 
     public var value: Int {
         get {
             lock.wait()
             defer { lock.signal() }
-            return localValue
+            return storedValue
         }
         set {
             lock.wait()
             defer { lock.signal() }
-            localValue = newValue
+            storedValue = newValue
         }
     }
 
     public func increment() {
         lock.wait()
         defer { lock.signal() }
-        localValue += 1
+        storedValue += 1
     }
 }
