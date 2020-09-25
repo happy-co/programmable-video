d2379da6baa132258aa75b25b50bea540a75c92d
John <jd.macpherson@gmail.com>
2020-09-25 06:23:27 +0000

    #35 Added Region enums for both `ConnectOptions.region` and `Room.mediaRegion` instead of string values

---
+++ programmable_video/lib/src/exceptions/missing_parameter_exception.dart
@@ -0,0 +1,16 @@
+part of twilio_programmable_video;
+
+class MissingParameterException extends PlatformException {
+  MissingParameterException({
+    @required String code,
+    String message,
+    dynamic details,
+  }) : super(
+          code: code,
+          message: message,
+          details: details,
+        );
+
+  @override
+  String toString() => 'MissingParameterException($code, $message, $details)';
+}
---
+++ programmable_video/lib/src/exceptions/not_found_exception.dart
@@ -0,0 +1,16 @@
+part of twilio_programmable_video;
+
+class NotFoundException extends PlatformException {
+  NotFoundException({
+    @required String code,
+    String message,
+    dynamic details,
+  }) : super(
+          code: code,
+          message: message,
+          details: details,
+        );
+
+  @override
+  String toString() => 'NotFoundException($code, $message, $details)';
+}
