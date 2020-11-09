8848d5a8f0e4e9f313af33ebc4aed08723992bfd
Adam Moyle <adam@happy.co>
2020-11-09 14:27:26 +1030

    Remove un-needed code from YuvFrame

--- android/src/main/kotlin/twilio/flutter/twilio_programmable_video/YuvFrame.java
+++ android/src/main/kotlin/twilio/flutter/twilio_programmable_video/YuvFrame.java
@@ -31,30 +31,7 @@ public class YuvFrame {
      */
     @SuppressWarnings("unused")
     public YuvFrame(final I420Frame i420Frame) {
-        fromI420Frame(i420Frame, PROCESSING_NONE, System.nanoTime());
-    }
-
-    /**
-     * Creates a YuvFrame from the provided I420Frame. Does any processing indicated, and uses the current time as a timestamp.
-     *
-     * @param i420Frame       Source I420Frame.
-     * @param processingFlags Processing flags, YuvFrame.PROCESSING_NONE for no processing.
-     */
-    @SuppressWarnings("unused")
-    public YuvFrame(final I420Frame i420Frame, final int processingFlags) {
-        fromI420Frame(i420Frame, processingFlags, System.nanoTime());
-    }
-
-    /**
-     * Creates a YuvFrame from the provided I420Frame. Does any processing indicated, and uses the given timestamp.
-     *
-     * @param i420Frame       Source I420Frame.
-     * @param processingFlags Processing flags, YuvFrame.PROCESSING_NONE for no processing.
-     * @param timestamp       The timestamp to give the frame.
-     */
-    @SuppressWarnings("unused")
-    public YuvFrame(final I420Frame i420Frame, final int processingFlags, final long timestamp) {
-        fromI420Frame(i420Frame, processingFlags, timestamp);
+        fromI420Frame(i420Frame);
     }
 
     /**
@@ -62,26 +39,16 @@ public class YuvFrame {
      * or will reuse existing arrays if they're already the correct size.
      *
      * @param i420Frame       Source I420Frame.
-     * @param processingFlags Processing flags, YuvFrame.PROCESSING_NONE for no processing.
-     * @param timestamp       The timestamp to give the frame.
      */
-    private void fromI420Frame(final I420Frame i420Frame, final int processingFlags, final long timestamp) {
+    private void fromI420Frame(final I420Frame i420Frame) {
         synchronized (planeLock) {
             try {
-
                 // Copy YUV stride information
                 int[] yuvStrides = new int[i420Frame.yuvStrides.length];
                 System.arraycopy(i420Frame.yuvStrides, 0, yuvStrides, 0, i420Frame.yuvStrides.length);
-
                 // Copy rotation information
                 rotationDegree = i420Frame.rotationDegree;  // Just save rotation info for now, doing actual rotation can wait until per-pixel processing.
-
-                // Copy the pixel data, processing as requested.
-                if (PROCESSING_CROP_TO_SQUARE == (processingFlags & PROCESSING_CROP_TO_SQUARE)) {
-                    copyPlanesCropped(i420Frame);
-                } else {
-                    copyPlanes(i420Frame);
-                }
+                copyPlanes(i420Frame);
             } catch (Throwable t) {
                 dispose();
             }
@@ -106,7 +73,7 @@ public class YuvFrame {
      */
     private void copyPlanes(final I420Frame i420Frame) {
         synchronized (planeLock) {
-            // Copy the Y, V, and U ButeBuffers to their corresponding byte arrays.
+            // Copy the Y, V, and U ByteBuffers to their corresponding byte arrays.
             // Existing byte arrays are passed in for possible reuse.
             yPlane = copyByteBuffer(yPlane, i420Frame.yuvPlanes[I420_Y]);
             vPlane = copyByteBuffer(vPlane, i420Frame.yuvPlanes[I420_V]);
@@ -141,147 +108,6 @@ public class YuvFrame {
         src.get(out);
 
         return out;
-    }
-
-    /**
-     * Copy the Y, V, and U planes from the source I420Frame, cropping them to square.
-     * Sets width and height.
-     *
-     * @param i420Frame Source frame.
-     */
-    private void copyPlanesCropped(final I420Frame i420Frame) {
-        synchronized (planeLock) {
-            // Verify that the dimensions of the I420Frame are appropriate for cropping
-            // If improper dimensions are found, default back to copying the entire frame.
-            final int width = i420Frame.width;
-            final int height = i420Frame.height;
-
-            if (width > height) {
-                // Calculate the size of the cropped portion of the the image
-                // The cropped width must be divisible by 4, since it will be divided by 2 to crop the center of the frame,
-                // and then divided by two again for processing the U and V planes, as each value there corresponds to
-                // a 2x2 square of pixels. All of those measurements must be whole integers.
-                final int cropWidth = width - height;
-                if ((cropWidth % 4) == 0) {
-                    // Create a row buffer for the crop method to use - the largest row width will be equal to the source frame's height (since we're cropping to square)
-                    final byte[] row = new byte[height];
-
-                    // Copy the Y plane.  Existing yPlane is passed in for possible reuse if it's the same size.
-                    yPlane = cropByteBuffer(yPlane, i420Frame.yuvPlanes[I420_Y], width, height, row);
-
-                    // Copy/crop the U and V planes. The U and V planes' width and height will be half that of the Y plane's.
-                    // The same row buffer can be reused, since being oversize isn't an issue.
-                    vPlane = cropByteBuffer(vPlane, i420Frame.yuvPlanes[I420_V], width / 2, height / 2, row);
-                    uPlane = cropByteBuffer(uPlane, i420Frame.yuvPlanes[I420_U], width / 2, height / 2, row);
-
-                    // Set size
-                    // noinspection SuspiciousNameCombination (Shut up, Lint, I know what I'm doing.)
-                    this.width = height;
-                    this.height = height;
-                } else {
-                    copyPlanes(i420Frame);
-                }
-            } else {
-                // Calculate the size of the cropped portion of the the image
-                // The cropped height must be divisible by 4, since it will be divided by 2 to crop the center of the frame,
-                // and then divided by two again for processing the U and V planes, as each value there corresponds to
-                // a 2x2 square of pixels. All of those measurements must be whole integers.
-                final int cropHeight = height - width;
-                if ((cropHeight % 4) == 0) {
-                    // Copy the Y plane. (No row buffer is needed if height >= width.)
-                    yPlane = cropByteBuffer(yPlane, i420Frame.yuvPlanes[I420_Y], width, height, null);
-
-                    // Copy/crop the U and V planes. The U and V planes' width and height will be half that of the Y plane's.
-                    // The same row buffer can be reused, since being oversize isn't an issue.
-                    vPlane = cropByteBuffer(vPlane, i420Frame.yuvPlanes[I420_V], width / 2, height / 2, null);
-                    uPlane = cropByteBuffer(uPlane, i420Frame.yuvPlanes[I420_U], width / 2, height / 2, null);
-
-                    // Set size
-                    // noinspection SuspiciousNameCombination (Shut up, Lint, I know what I'm doing.)
-                    this.height = width;
-                    this.width = width;
-                } else {
-                    copyPlanes(i420Frame);
-                }
-            }
-        }
-    }
-
-    /**
-     * Copies the contents of a ByteBuffer into a byte array, cropping the center of the image to square.
-     * If the byte array exists, and is the correct size, it will be reused.
-     * If the byte array is null, or isn't properly sized, a new byte array will be created.
-     *
-     * @param dst       A byte array to copy the ByteBuffer contents to. Can be null.
-     * @param src       A ByteBuffer to copy data from.
-     * @param srcWidth  The width of the source frame.
-     * @param srcHeight The height of ths source frame.
-     * @param row       A byte array with a length equal to or greater than the cropped frame's width, for use as a buffer.
-     *                  Can be null. If no row buffer is provided and one is needed, or the buffer is too short, an exception
-     *                  will likely result.
-     * @return A byte array containing the cropped contents of the ByteBuffer. If the provided dst was non-null and the correct size,
-     * it will be returned. If not, a new byte array will be created.
-     * @throws NullPointerException
-     */
-    private byte[] cropByteBuffer(@Nullable byte[] dst, @NonNull final ByteBuffer src, final int srcWidth, final int srcHeight, @Nullable final byte[] row)
-            throws NullPointerException {
-        // If the frame is wider than it is tall, copy the center of each row to trim off the left and right edges
-        if (srcWidth > srcHeight) {
-            // We'll need a row buffer, here. Throw an exception if we don't have one.
-            if (null == row) {
-                throw new NullPointerException("YuvFrame.cropByteBffer: Need row buffer array, and the array provided was null.");
-            }
-
-            // Create a new destination byte array if necessary.
-            final int croppedSize = srcHeight * srcHeight;
-            final byte[] out;
-            if ((null == dst) || (dst.length != croppedSize)) {
-                out = new byte[croppedSize];
-            } else {
-                out = dst;
-            }
-
-            // Calculate where on each row to start copying
-            final int indent = (srcWidth - srcHeight) / 2;
-
-            // Copy the ByteBuffer
-            for (int i = 0; i < srcHeight; i++) {
-                // Set the position of the ByteBuffer to the beginning of the current row,
-                // adding the calculated indent to trim off the left side.
-                src.position((i * srcWidth) + indent);
-
-                // Copy the cropped row to the row buffer
-                src.get(row, 0, srcHeight);
-
-                // Copy the row buffer to the destination array
-                System.arraycopy(row, 0, out, i * srcHeight, srcHeight);
-            }
-
-            return out;
-        }
-        // If the frame is taller than it is wide, copy the center of the image, cropping off the top and bottom edges.
-        // NOTE: If the width and height are equal, this method should result in a straight copy of the source ByteBuffer,
-        //       as the calculated row offset will be zero.
-        else {
-            // Create a new destination byte array if necessary.
-            final int croppedSize = srcWidth * srcWidth;
-            final byte[] out;
-            if ((null == dst) || (dst.length != croppedSize)) {
-                out = new byte[croppedSize];
-            } else {
-                out = dst;
-            }
-
-            // Calculate where to start reading
-            final int start = ((srcHeight - srcWidth) / 2) * srcWidth;  // ((h-w)/2) is the number of rows to skip, multiply by w again to get the starting ByteBuffer position.
-
-            // Copy the ByteBuffer
-            // Since we need to take a sequential series of whole rows, only one copy is necessary
-            src.position(start);
-            src.get(out, 0, croppedSize);
-
-            return out;
-        }
     }
 
     /**
