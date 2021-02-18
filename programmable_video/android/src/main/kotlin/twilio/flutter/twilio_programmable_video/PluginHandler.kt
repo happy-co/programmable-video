44f24bed1f319fb4f09f2613bf52af33f385df93
Sam Fong <sam.fong@happy.co>
2021-02-18 15:55:21 +1030

    Temporarily disable Camera2Capturer
    Camera2Capturer is not compatible with our photo capture renderer.
    
    Also, fixed a minor issue with reset video causing an error when
    disconnecting.

--- programmable_video/android/src/main/kotlin/twilio/flutter/twilio_programmable_video/PluginHandler.kt
+++ programmable_video/android/src/main/kotlin/twilio/flutter/twilio_programmable_video/PluginHandler.kt
@@ -139,12 +139,12 @@ class PluginHandler : MethodCallHandler, ActivityAware, BaseListener {
     private fun localParticipantResetVideo(call: MethodCall, result: MethodChannel.Result) {
         TwilioProgrammableVideoPlugin.debug("PluginHandler.localParticipantResetVideo => called")
         val localVideoTrack = getLocalParticipant()?.localVideoTracks?.firstOrNull()
-        if (localVideoTrack != null) {
+        if (localVideoTrack != null && localVideoTrack.localVideoTrack.isEnabled) {
             getLocalParticipant()?.unpublishTrack(localVideoTrack.localVideoTrack)
             getLocalParticipant()?.publishTrack(localVideoTrack.localVideoTrack)
             return result.success(true)
         }
-        return result.error("NOT_FOUND", "No LocalVideoTrack found", null)
+        return result.success(false)
     }
 
     private fun localVideoTrackEnable(call: MethodCall, result: MethodChannel.Result) {
